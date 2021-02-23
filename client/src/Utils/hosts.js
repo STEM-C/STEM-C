598d8b37ef00753b373c965215ff110ae4e57106
MeghaNagarmunoli <meghanagarmunoli@gmail.com>
2021-02-23 02:30:18 -0500

    Updated broken workspace for the coding challenge

--- client/src/Utils/hosts.js
+++ client/src/Utils/hosts.js
@@ -2,4 +2,5 @@
 const { hostname } = window.location
 
 // export the server host
-export const server = hostname.includes('localhost') ? 'http://localhost:1337/api' : '/api'
+// export const server = hostname.includes('localhost') ? 'http://localhost:1337/api' : '/api'
+export const server = 'https://casmm-staging.herokuapp.com/api'
--- client/src/views/Challenge/Challenge.js
+++ client/src/views/Challenge/Challenge.js
@@ -1,37 +1,63 @@
 import React, {useState, useEffect} from "react"
-import {Link} from "react-router-dom"
 import './Challenge.css'
+import {postJoin} from "../../Utils/requests";
+import {message} from "antd";
+import {setUserSession} from "../../Utils/AuthRequests";
+import '../Student/Student.js'
 
 // Use this for http requests
 import axios from 'axios'
 
 function Challenge(props) {
-    const [topics, setTopics] = useState([])
+    const [learningStandard, setLearningStandard] = useState({});
 
     useEffect( () => {
-        // Implement the fetchTopics function
-        const fetchTopics = async () => {
+        // Complete the fetchClass function 
+        const fetchClass = async () => {
+            
+            // Basic authetication required
+            let joinCode = '0450';
+            let ids = [2, 3];
+            const res = await postJoin(joinCode, ids);
+            if (res.data) {
+                setUserSession(res.data.jwt, JSON.stringify(res.data.students));
+            } else {
+                message.error(res.err);
+            }
+            
+
             // Call the topics api
-            // Set the topics state with the response data
+            // Set the learningStandard state with the response data
+
+
         }
 
-        fetchTopics()
+        fetchClass()
     }, [])
 
-    // Render the topics
+
+
+    const handleSelection = (day) => {
+        //Sets the selected day in the localStorage that workspace uses.
+        localStorage.setItem("my-day", JSON.stringify(day));
+
+    };
+
+    // Render the LearningStandards
     return (
-        // For each topic in the array, render a row with its name and description
+        // For each Learning Standard in the response, render a row with its name and expectations.
 
-        // For each activity belonging to a topic, render a row with its name, difficulty, and learning category
+        // For each day belonging to a learningStandard, render a row with its number
 
-        // Each activity row should be wrapped in a link that will take a user to the workspace (this is already implemented for you)
-        // <Link to={`workspace/${topics[i].activities[i].name}`} onClick={() => props.setSelectedActivity(topics[i].activities[i])}>
+        // Each day row should be wrapped in a link that will take a user to the workspace (this is already implemented for you)
+        // <Link to={`workspace/`} onClick={() => handleSelection(learningStandard[i].days[j])}>
         //     Your activity code here...
         // </Link>
 
-        // Bonus: Only show a topic's activities on hover
+        // Bonus: Only show a LearningStandards's days on hover
 
         null
+
     )
 }
 
